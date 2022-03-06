2cb7b0363dc99fcfcbcf3091a717fd1ca057f64e
21ciprian <2021ciprianoffice@gmail.com>
2022-03-06 00:30:03 +0000

    Declared input and search button variables

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('test');
+"use strict";
+const searchButton = document.querySelector('#search');
+const nameInput = document.querySelector('#nameInput');
+// nameInput?.addEventListener('input', () =>
+// 	console.log('nameInput: ', nameInput.value)
+// )
+// console.log('searchButton: ', searchButton.innerText)
--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-console.log('test')
+const searchButton = <HTMLButtonElement>document.querySelector('#search')
+const nameInput = <HTMLInputElement>document.querySelector('#nameInput')
+
+// nameInput?.addEventListener('input', () =>
+// 	console.log('nameInput: ', nameInput.value)
+// )
+// console.log('searchButton: ', searchButton.innerText)
