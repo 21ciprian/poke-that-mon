0b66ee455a0b587446e84683ffc8c553289b460d
21ciprian <2021ciprianoffice@gmail.com>
2022-03-06 01:36:08 +0000

    Created fetch pokemon by id function

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const searchButton = document.querySelector('#search');
 const nameInput = document.querySelector('#nameInput');
-// nameInput?.addEventListener('input', () =>
-// 	console.log('nameInput: ', nameInput.value)
-// )
+nameInput === null || nameInput === void 0 ? void 0 : nameInput.addEventListener('input', () => console.log('nameInput: ', nameInput.value));
 // console.log('searchButton: ', searchButton.innerText)
+const pokemonsContainer = document.getElementById('pokemonContainer');
+const pokemonModal = document.querySelector('#pokemonModal');
+const pokemon_number = 20;
+function getPokemonById(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const response = yield fetch(url);
+        const pokemon = yield response.json();
+        console.log('pokemon: ', pokemon);
+        // createMiniPokemonCard(pokemon);
+    });
+}
+getPokemonById(2);
--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
 const searchButton = <HTMLButtonElement>document.querySelector('#search')
 const nameInput = <HTMLInputElement>document.querySelector('#nameInput')
 
-// nameInput?.addEventListener('input', () =>
-// 	console.log('nameInput: ', nameInput.value)
-// )
+nameInput?.addEventListener('input', () =>
+	console.log('nameInput: ', nameInput.value)
+)
 // console.log('searchButton: ', searchButton.innerText)
+
+const pokemonsContainer = document.getElementById('pokemonContainer')
+const pokemonModal = document.querySelector('#pokemonModal')
+const pokemon_number: number = 20
+async function getPokemonById(id: number): Promise<void> {
+	const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+	const response = await fetch(url)
+	const pokemon = await response.json()
+	console.log('pokemon: ', pokemon)
+
+	// createMiniPokemonCard(pokemon);
+}
+getPokemonById(2)
