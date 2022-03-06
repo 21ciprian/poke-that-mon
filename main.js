174de96a5f1317d3d27ff6020c4faf996fd07ee4
21ciprian <2021ciprianoffice@gmail.com>
2022-03-06 03:27:26 +0000

    Created create pokemon card function

--- main.js
+++ main.js
@@ -10,11 +10,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 const searchButton = document.querySelector('#search');
 const nameInput = document.querySelector('#nameInput');
-nameInput === null || nameInput === void 0 ? void 0 : nameInput.addEventListener('input', () => console.log('nameInput: ', nameInput.value));
-// console.log('searchButton: ', searchButton.innerText)
-const pokemonsContainer = document.getElementById('pokemonContainer');
+const pokemonsContainer = (document.querySelector('#pokemonContainer'));
+console.log('pokemonsContainer: ', pokemonsContainer);
 const pokemonModal = document.querySelector('#pokemonModal');
-const pokemon_number = 20;
+const pokemon_number = 10;
 function getPokemonsList() {
     return __awaiter(this, void 0, void 0, function* () {
         for (let i = 1; i <= pokemon_number; i++) {
@@ -27,8 +26,43 @@ function getPokemonById(id) {
         const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
         const response = yield fetch(url);
         const pokemon = yield response.json();
-        console.log('pokemon: ', pokemon.name);
-        // createMiniPokemonCard(pokemon);
+        console.log('pokemon: ', pokemon);
+        createMiniPokemonCard(pokemon);
     });
+}
+function createMiniPokemonCard(pokemon) {
+    const poke_types = pokemon.types.map(type => type.type.name);
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const miniPokemonCard = document.createElement('div');
+    miniPokemonCard.classList.add('miniPokemonCard');
+    miniPokemonCard.setAttribute('aria-label', pokemon.name);
+    const imgContainer = document.createElement('div');
+    const infoContainer = document.createElement('div');
+    infoContainer.classList.add('info');
+    infoContainer.setAttribute('aria-label', pokemon.name);
+    imgContainer.classList.add('img-container');
+    imgContainer.setAttribute('aria-label', pokemon.name);
+    const pokeImg = document.createElement('img');
+    pokeImg.setAttribute('aria-label', pokemon.name);
+    pokeImg.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`);
+    pokeImg.setAttribute('alt', pokemon.name);
+    const idPokemon = document.createElement('span');
+    idPokemon.setAttribute('aria-label', pokemon.name);
+    idPokemon.classList.add('number');
+    idPokemon.innerText = `#${pokemon.id.toString().padStart(3, '0')}`;
+    const pokemonName = document.createElement('h3');
+    pokemonName.setAttribute('aria-label', pokemon.name);
+    pokemonName.classList.add('name');
+    pokemonName.innerText = name;
+    const pokemonType = document.createElement('small');
+    pokemonType.classList.add('type');
+    pokemonType.innerText = `Type: `;
+    const spanType = document.createElement('span');
+    spanType.innerText = poke_types[0];
+    pokemonType.appendChild(spanType);
+    imgContainer.appendChild(pokeImg);
+    infoContainer.append(idPokemon, pokemonName, pokemonType);
+    miniPokemonCard.append(imgContainer, infoContainer);
+    pokemonsContainer.appendChild(miniPokemonCard);
 }
 getPokemonsList();
--- main.ts
+++ main.ts
@@ -1,15 +1,117 @@
+type PAbility = {
+	ability: {
+		name: string
+		url: string
+	}
+	is_hidden: boolean
+	slot: number
+}
+type PForm = {
+	name: string
+	url: string
+}
+type PGroupDetail = {
+	level_learned_at: number
+	move_learn_method: {
+		name: string
+		url: string
+	}
+	version_group: {
+		name: string
+		url: string
+	}
+}
+type PSpecie = {
+	name: string
+	url: string
+}
+type PIndices = {
+	game_index: number
+	version: {
+		name: string
+		url: string
+	}
+}
+type PSprite = {
+	back_default: string
+	back_female: string | null
+	back_shiny: string
+	back_shiny_female: string | null
+	front_default: string
+	front_female: string | null
+	front_shiny: string
+	front_shiny_female: string | null
+	other: {
+		dream_world: {
+			back_default: string
+			front_female: string | null
+		}
+		home: {
+			front_default: string
+			front_female: string | null
+			back_shiny: string
+			front_shiny_female: string | null
+		}
+		'official-artwork': {
+			front_default: string
+		}
+	}
+	versions: {}
+}
+type PType = {
+	slot: number
+	type: {
+		name: string
+		url: string
+	}
+}
+type PStat = {
+	base_stat: number
+	effort: number
+	stat: {
+		name: string
+		url: string
+	}
+}
+type PMove = {
+	move: {
+		name: string
+		url: string
+	}
+	version_group_details: PGroupDetail[]
+}
+type PProps = {
+	abilities: PAbility[]
+	base_experience: number
+	forms: PForm[]
+	game_indices: PIndices[]
+	height: number
+	held_items: any[]
+	id: number
+	is_default: boolean
+	location_area_encounters: string
+	moves: PMove[]
+	name: string
+	order: number
+	past_types: any[]
+	species: PSpecie
+	sprites: any
+	stats: PStat[]
+	types: PType[]
+	weight: number
+}
 const searchButton = <HTMLButtonElement>document.querySelector('#search')
 const nameInput = <HTMLInputElement>document.querySelector('#nameInput')
 
-nameInput?.addEventListener('input', () =>
-	console.log('nameInput: ', nameInput.value)
+const pokemonsContainer = <HTMLElement>(
+	document.querySelector('#pokemonContainer')
 )
-// console.log('searchButton: ', searchButton.innerText)
-
-const pokemonsContainer = document.getElementById('pokemonContainer')
+console.log('pokemonsContainer: ', pokemonsContainer)
 const pokemonModal = document.querySelector('#pokemonModal')
-const pokemon_number: number = 20
-async function getPokemonsList() {
+
+const pokemon_number: number = 10
+
+async function getPokemonsList(): Promise<void> {
 	for (let i = 1; i <= pokemon_number; i++) {
 		await getPokemonById(i)
 	}
@@ -18,8 +120,57 @@ async function getPokemonById(id: number): Promise<void> {
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 	const response = await fetch(url)
 	const pokemon = await response.json()
-	console.log('pokemon: ', pokemon.name)
+	console.log('pokemon: ', pokemon)
 
-	// createMiniPokemonCard(pokemon);
+	createMiniPokemonCard(pokemon)
+}
+function createMiniPokemonCard(pokemon: PProps) {
+	const poke_types = pokemon.types.map(type => type.type.name)
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+	const miniPokemonCard = <HTMLDivElement>document.createElement('div')
+
+	miniPokemonCard.classList.add('miniPokemonCard')
+	miniPokemonCard.setAttribute('aria-label', pokemon.name)
+
+	const imgContainer = <HTMLDivElement>document.createElement('div')
+	const infoContainer = <HTMLDivElement>document.createElement('div')
+	infoContainer.classList.add('info')
+	infoContainer.setAttribute('aria-label', pokemon.name)
+
+	imgContainer.classList.add('img-container')
+	imgContainer.setAttribute('aria-label', pokemon.name)
+
+	const pokeImg = <HTMLImageElement>document.createElement('img')
+	pokeImg.setAttribute('aria-label', pokemon.name)
+	pokeImg.setAttribute(
+		'src',
+		`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+	)
+	pokeImg.setAttribute('alt', pokemon.name)
+
+	const idPokemon = <HTMLSpanElement>document.createElement('span')
+	idPokemon.setAttribute('aria-label', pokemon.name)
+	idPokemon.classList.add('number')
+	idPokemon.innerText = `#${pokemon.id.toString().padStart(3, '0')}`
+
+	const pokemonName = <HTMLHeadingElement>document.createElement('h3')
+	pokemonName.setAttribute('aria-label', pokemon.name)
+	pokemonName.classList.add('name')
+	pokemonName.innerText = name
+
+	const pokemonType = <HTMLElement>document.createElement('small')
+	pokemonType.classList.add('type')
+	pokemonType.innerText = `Type: `
+
+	const spanType = <HTMLSpanElement>document.createElement('span')
+	spanType.innerText = poke_types[0]
+
+	pokemonType.appendChild(spanType)
+
+	imgContainer.appendChild(pokeImg)
+	infoContainer.append(idPokemon, pokemonName, pokemonType)
+	miniPokemonCard.append(imgContainer, infoContainer)
+
+	pokemonsContainer.appendChild(miniPokemonCard)
 }
 getPokemonsList()
